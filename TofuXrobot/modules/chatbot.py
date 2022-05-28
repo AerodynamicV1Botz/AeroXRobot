0db7656ff9c8ea21c9486855b56d1d80bf2382bf
Tofu <91769994+Awesome-Tofu@users.noreply.github.com>
2022-05-28 13:55:13 +0530

    Update chatbot.py

--- TofuXrobot/modules/chatbot.py
+++ TofuXrobot/modules/chatbot.py
@@ -39,7 +39,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Miku Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
+                "Tofu Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -63,7 +63,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Miku Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "Tofu Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
